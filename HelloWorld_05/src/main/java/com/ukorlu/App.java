f18b754ad64dde92edf62090d2b99d29248e914d
unsalk <unsalk@OZUIT-UK.ozun.int>
2019-08-05 16:19:31 +0300

    test

---
+++ HelloWorld_05/src/main/java/com/ukorlu/App.java
@@ -0,0 +1,19 @@
+package com.ukorlu;
+
+import com.ukorlu.utility.FileUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		StringBuilder builder = new StringBuilder();
+		for (String string : args) {
+			builder.append(string + "\r\n");
+		}
+		FileUtil.writeFile("C:/data/out.txt", builder.toString());
+
+	}
+}
